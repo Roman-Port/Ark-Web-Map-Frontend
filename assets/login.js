6e1273b5d03b6237da6268460314209b0533147c
Roman-Port <rvporterfield@gmail.com>
2020-05-17 15:05:17 -0500

    Added beta key support in login

--- assets/login.js
+++ assets/login.js
@@ -4,7 +4,23 @@ if (args["next"] != null) {
     next = args["next"];
 }
 
-function OpenLoginPrompt() {
+function VerifyBetaKey() {
+    var key = document.getElementById('beta_key_entry').value;
+    SetBetaError("");
+    delta.serverRequest(LAUNCH_CONFIG.API_ENDPOINT + "/auth/validate_beta_key?beta_key=" + encodeURIComponent(key), {}, (d) => {
+        if (d.ok) {
+            OpenLoginPrompt(key);
+        } else {
+            SetBetaError("Invalid Beta Key");
+        }
+    });
+}
+
+function SetBetaError(text) {
+    document.getElementById('beta_key_error').innerText = text;
+}
+
+function OpenLoginPrompt(key) {
     //Create nonce
     var nonce = Math.floor(Math.random() * 999999).toString();
 
@@ -12,8 +28,11 @@ function OpenLoginPrompt() {
     localStorage.setItem("login_latest_nonce", nonce)
 
     //Go
-    var url = LAUNCH_CONFIG.API_ENDPOINT + "/auth/steam_auth?next=" + encodeURIComponent(next) + "&type=" + LAUNCH_CONFIG.LOGIN_ENVIORNMENT_ID + "&nonce=" + nonce;
+    var url = LAUNCH_CONFIG.API_ENDPOINT + "/auth/steam_auth?next=" + encodeURIComponent(next) + "&type=" + LAUNCH_CONFIG.LOGIN_ENVIORNMENT_ID + "&nonce=" + nonce + "&beta_key=" + encodeURIComponent(key);
     window.location = url;
 }
 
-document.getElementById('login_box_btn').addEventListener("click", OpenLoginPrompt);
+document.getElementById('login_box_btn').addEventListener("click", VerifyBetaKey);
+document.getElementById('beta_key_entry').addEventListener("input", () => {
+    SetBetaError("");
+});
--- login/index.php
+++ login/index.php
@@ -11,6 +11,11 @@
         <div class="login_box_title">Welcome to Delta Web Map!</div>
         <div class="login_box_text">To gather info, you'll need to log in with Steam</div>
         <div class="login_box_text">By signing in, you agree to our <a href="/privacy/" target="_blank">Privacy Policy</a></div>
+        <div class="beta_key_container">
+            <div class="beta_key_text">Beta Key</div>
+            <input class="beta_key_input" type="text" placeholder="AAAA-BBBB-CCCC" id="beta_key_entry" />
+            <div class="beta_key_text" style="color:#f16868;" id="beta_key_error"></div>
+        </div>
         <div class="login_box_btn" id="login_box_btn">Sign In with Steam</div>
     </div>
 
